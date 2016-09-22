fa02093776b5b85e2e0976c486d61b13d39c4c77
asolasconsalas <asolasconsalas@gmail.com>
2016-09-21 23:46:37 -0500

    2016-09-21
    Primero

---
+++ howAreYou.rb
@@ -0,0 +1,6 @@
+puts 'imprimiendo en Ruby'
+puts 'whats your name?'
+name=gets.chomp
+
+puts name
+p '********'
---
+++ number.rb
@@ -0,0 +1,20 @@
+
+
+turn1, turn2, turn3 = ARGV
+
+
+
+
+puts 'Number 1: '
+a = gets.chomp.to_i
+
+puts 'Number 2:'
+b = gets.chomp.to_i
+
+puts a.class
+puts b.class
+
+puts "La suma es: #{a + b}"
+puts "La resta es: #{a - b}"
+puts "La multiplicacion es: #{a * b}"
+puts "La division es: #{a / b}"
